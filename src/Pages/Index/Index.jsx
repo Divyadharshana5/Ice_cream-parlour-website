90194d330844bcba782bddd6a84c850f663aa905
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-13 15:04:50 +0530

    Update index.jsx

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import heroImage from "./../../assets/rev_home3_1.png";
 
 const Index = () => {
   return (
-    <>
+    <div className="flex flex-col gap-[20px] relative min-h-[90vh] bg-[#ffded1]">
       <Navbar />
-    </>
+
+      <div className="">
+        <div className=""></div>
+        <div className="">
+          <img src={heroImage} alt="" />
+        </div>
+      </div>
+    </div>
   );
 };
 
